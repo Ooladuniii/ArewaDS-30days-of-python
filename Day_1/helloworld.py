cd140c35ed05bef8f656c64bd252dad2cb4c26f0
Ooladuniii <olayinkamojisola94@gmail.com>
2023-12-03 21:55:02 +0100

    Remove 30-Days-of-Python/

---
+++ Day_1/helloworld.py
@@ -0,0 +1,118 @@
+# Exercise: Level 2
+
+# check python version
+import platform
+
+print("Python Version:")
+print(platform.python_version())
+
+# Addition
+add_num = 3 + 4 
+
+print( add_num)
+
+# substraction
+
+sub_num = 3 - 4 
+
+print( sub_num)
+
+# Multiplication
+
+mul_num = 3 * 4
+
+print( mul_num)
+
+# Modulus
+
+mod = 3 % 4
+
+print(mod)
+
+# Division
+
+div = 3 / 4 
+
+print(div)
+
+# Exponetial
+
+exp = 3** 4
+
+print(exp)
+
+# Floor division
+
+floor_div = 3//4
+
+print(floor_div)
+
+
+# Strings
+My_name = "Olayinka Mojisola Babalola"
+
+print(My_name)
+
+Fam_name = "Babalola"
+
+print(Fam_name)
+
+My_country = "Nigeria"
+
+print (My_country)
+
+fav = "I am enjoying 30 days of Python"
+
+print (fav)
+
+# checking data types 
+number = 10
+
+data_type = type(number)
+
+print(data_type)
+number = 9.8
+
+data_type = type(number)
+
+print(data_type)
+
+number = 3.14
+
+data_type = type(number)
+
+print(data_type)
+
+number = 4-4j
+
+data_type = type(number)
+
+print(data_type)
+
+data = ['Asabeneh', 'Python', 'Finland']
+
+data_type = type(data)
+
+print(data_type)
+
+My_name = "Olayinka Mojisola Babalola"
+
+data_type = type(My_name)
+
+print(data_type)
+
+Fam_name = "Babalola"
+
+data_type = type(Fam_name)
+
+print(data_type)
+
+My_country = "Nigeria"
+
+data_type = type(My_country)
+
+print(data_type)
+
+
+
+
